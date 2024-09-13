b83bfa3127b40ce8783082f6e256cc967d7eb455
den-bulaev <denisbulaev7@gmail.com>
2024-09-13 15:05:07 +0300

    loader centered

--- src/components/loaderComponent/LoaderComponent.tsx
+++ src/components/loaderComponent/LoaderComponent.tsx
@@ -1,6 +1,6 @@
 const LoaderComponent: React.FC = () => {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center absolute top-0 left-0 right-0 bottom-0">
       <h1 className="text-3xl">Loading...</h1>
     </div>
   );
